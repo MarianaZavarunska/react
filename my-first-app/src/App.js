8530172e74ef16c84ffaf3bec71bcc6c60324070
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-01-27 18:27:28 +0200

    initial coment

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Counter from "./components/Counter/Counter";
+
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <>
-      <Counter />
+      <Form />
     </>
   );
 }
---
+++ my-first-app/src/components/Form/Form.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Form.css";
+
+const Form = () => {
+    const []
+  return (
+    <div className="wrapper">
+      <div>
+        <form>
+          <label>Add dog:</label>
+          <input type="text" />
+        </form>
+      </div>
+      <div>
+        <form>
+          <label>Add cat:</label>
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;

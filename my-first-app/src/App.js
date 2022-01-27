4e71b6cae118b77f40c10d2845f868f1ec03cfaa
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-01-27 15:01:15 +0200

    homework was done

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
-import Users from "./components/Users/Users";
-import Posts from "./components/Posts/Posts";
-import Comments from './components/Comments/Comments';
+import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
-    <div>
-      <div className="main">
-      <div className="users-wrapper"><Users /></div>
-      <div className="posts-wrapper"><Posts /></div>
-      </div>
-      <div className="footer">
-        <Comments/>
-      </div>
-   
-    
-    </div>
-  )
-  
+    <>
+      <Counter />
+    </>
+  );
 }
 
 export default App;

53f194ae6e493c7bd325a2a9bfcc994598dca471
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-02-03 18:31:42 +0200

    fixed interface for obj

--- my-first-app/interfaces/space.interface.js
+++ my-first-app/interfaces/space.interface.js
@@ -1,2 +1,19 @@
 "use strict";
+// export interface ISpace {
+//         mission_name: string,
+//         launch_date_local: string,
+//         launch_site: {
+//             site_name_long: string
+//         },
+//         links: {
+//             article_link: null;
+//             video_link: string
+//         },
+//         rocket: {
+//             rocket_name: string;
+//             first_stage:  {
+//                 cores:Icores
+//             };
+//             second_stage: ISecondStage;
+//         }
 exports.__esModule = true;
--- my-first-app/interfaces/space.interface.ts
+++ my-first-app/interfaces/space.interface.ts
@@ -1,30 +1,69 @@
-export interface ISpace {
-        mission_name: string,
-        launch_date_local: string,
-        launch_site: {
-            site_name_long: string
-        },
-        links: {
-            article_link: null;
-            video_link: string
-        },
-        rocket: {
-            rocket_name: string;
-            first_stage:  IFirstStage;
-            second_stage: ISecondStage;
-        }
+// export interface ISpace {
+//         mission_name: string,
+//         launch_date_local: string,
+//         launch_site: {
+//             site_name_long: string
+//         },
+//         links: {
+//             article_link: null;
+//             video_link: string
+//         },
+//         rocket: {
+//             rocket_name: string;
+//             first_stage:  {
+//                 cores:Icores
+//             };
+//             second_stage: ISecondStage;
+//         }
     
+// }
+
+// interface  Icores {
+//     [index: number]:IFirstStage;
+//   }
+
+// interface IFirstStage
+//    {flight: number; core: { reuse_count: number; status: string;} }
+    
+
+
+// interface ISecondStage {
+//     payloads:  { payload_type: string;payload_mass_kg: number;payload_mass_lbs: number;}[]
+// }
+
+export interface ISpace {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: null;
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string;
+        first_stage:  {
+            cores:IFirstStage;
+        };
+        second_stage: ISecondStage;
+    }
+
 }
 
 interface IFirstStage{
-    cores:  {flight: number; core: { reuse_count: number; status: string;} }[]
-    
+       flight: number; 
+       core: { reuse_count: number; status: string;}
 }
-
 interface ISecondStage {
-    payloads:  { payload_type: string;payload_mass_kg: number;payload_mass_lbs: number;}[]
+    payloads:  { 
+        payload_type: string;
+        payload_mass_kg: number;
+        payload_mass_lbs: number;
+    }
 }
 
+//User for function
 export interface IUser{
     name: string;
     age: number;

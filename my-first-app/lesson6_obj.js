0f626666d38fac190bdc1f9622f77ccae5f7efa5
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2022-02-03 15:12:35 +0200

    initial comment

---
+++ my-first-app/lesson6_obj.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var space = { mission_name: 'Starlink-15 (v1.0)', launch_date_local: '2020-10-24T11:31:00-04:00' };
+console.log(space.mission_name);
---
+++ my-first-app/lesson6_obj.ts
@@ -0,0 +1,4 @@
+import {ISpace} from './interfaces/space.interface'
+const space: Partial<ISpace> = {mission_name:'Starlink-15 (v1.0)', launch_date_local: '2020-10-24T11:31:00-04:00',rocket.first_stage.cores.flight:12}
+
+console.log(space.mission_name);
